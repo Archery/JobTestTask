88c1e2ffcfc80f697ab4377e5a31be72acb83d6c
Archery <archerymask@gmail.com>
2022-06-10 22:02:28 +0300

    Added unit tests

---
+++ DividendsCalculationTests/DividendsManagerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DividendsCalculation.Tests {
+    [TestClass()]
+    public class DividendsManagerTests {
+        [TestMethod()]
+        public void DividendsManagerTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod,
+         DataRow(null, null, 100000, 0.1, 20, 124668.85)]
+        public void CalculateTotalInterestTest(DateTime? start_date, DateTime? calc_date, double X, double R, int N, double result) {
+            start_date ??= DateTime.Today;
+            calc_date ??= DateTime.Today;
+
+            var dm = new DividendsManager() {
+                AgreementDate = start_date.Value,
+                InvestedSum = X,
+                InvestmentDuration = N,
+                InterestRate = R,
+            };
+
+            Assert.AreEqual(result, dm.CalculateTotalInterest(calc_date.Value));
+            Assert.Fail();
+        }
+    }
+}
